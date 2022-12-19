3dc309d5e73360e88b4ce9bab676c331b8b25299
Husan19012 <121026490+Husan19012@users.noreply.github.com>
2022-12-19 15:51:42 +0500

    qwe
    qwe

---
+++ INDEX/1-dars/husan.js
@@ -0,0 +1,20 @@
+// let name = "husan"
+// names= "hasan"
+// document .write(names)
+// const number = 12
+// console .log(number+12)
+// alert("Salom") muloqot
+// confirm("salom") ok ignore
+prompt("Salom") so'rovnoma
+let job = prompt("You tube")
+console.log("https://www.youtube.com/user/" ${job}) 
+// let x=50
+// let y=100
+// console .log(a);
+
+//  let v = null
+// string
+// number
+// Bollean
+// null
+// under fined
---
+++ INDEX/2-dars/husan.js
@@ -0,0 +1,90 @@
+// // ildiz oqi
+// let x= Math.sqrt(12.6)  
+// console .log(x)
+
+
+// // daraja qiladi
+// let a =Math .round(5.6)
+// console .log(x)
+
+// // random qiladi
+// let c=Math.pow(12 ,12)
+// console .log(c)
+
+// let v =Math.random(12,34,54,56) *10
+// console .log(v)
+
+// let q =Math.floor(12)
+// console .log(q)
+// // musbat qiladi
+// let s =Math.abs(-67.5)
+// console .log(s)
+
+
+// let phones = {
+//     model:"Iphone 11",
+//     momory:128,
+//     clor:"purple"
+
+// }      // object
+// console .log(phones)
+
+// let object ={
+//    phones:{
+// model:"Iphone 11",
+// memory:128,
+// color:"purple"
+//     },
+//     phones2: {
+//         model:"Iphone 12",
+//         memory:128,
+//         color:"purple"
+//             },
+//             phones3:{
+//                 model:"Iphone 13",
+//                 memory:128,
+//                 color:"purple"
+//                     },
+//                     phones4:{
+//                         model:"Iphone 14",
+//                         memory:128,
+//                         color:"purple"
+//                             }
+// }
+
+
+// console .log(object.phones.color);
+
+let object ={
+    phones:{
+ model:"oq koylak",
+ memory:120,
+ color:"purple"
+     },
+     phones2: {
+         model:"fudbolka",
+         memory:70,
+         color:"blak"
+             },
+             phones3:{
+                 model:"kurtka",
+                 memory:130,
+                 color:"purple"
+                     },
+                     phones4:{
+                         model:"mayka",
+                         memory:100,
+                         color:"red"
+                             }
+ }
+ 
+ 
+ console .log(object.phones.color);
+ 
+
+
+
+
+
+
+
